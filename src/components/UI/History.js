3272ff9a964db11ac94a41f7ef164e3f4e7df979
ubairnoor <ubairnoor5@gmail.com>
2022-01-11 22:27:00 +0530

    History[Route Added & Design Added ]

---
+++ src/components/UI/History.js
@@ -0,0 +1,59 @@
+import { Box,Container,Grid,Card,CardHeader,CardMedia} from '@material-ui/core';
+import IconButton from '@mui/material/IconButton';
+import CancelIcon from '@mui/icons-material/Cancel';
+import colors from '../../colors';
+import React from 'react';
+const History = ()=>{
+    return (
+        <Box mt={15} mb={30}>
+            <Container maxWidth="lg">
+                <Grid container spacing={4}>
+                    <Grid item  sm="3" md="3" xs="12">
+                        <Card>
+                            <CardHeader  title="Rafta Rafta"  subheader="T series" action={
+                                <IconButton>
+                                    <CancelIcon  sx={{color:colors.primaryColor.color}}/>
+                                </IconButton>
+                            } />
+                            <CardMedia component="img" image="https://img.youtube.com/vi/B-J_PuEhyOM/maxresdefault.jpg"/>
+                        
+                        </Card>
+                    </Grid>
+                    <Grid item  sm="3">
+                        <Card>
+                            <CardHeader  title="Rafta Rafta"  subheader="T series" action={
+                                <IconButton>
+                                    <CancelIcon  sx={{color:colors.primaryColor.color}}/>
+                                </IconButton>
+                            } />
+                            <CardMedia component="img" image="https://img.youtube.com/vi/B-J_PuEhyOM/maxresdefault.jpg"/>
+                        
+                        </Card>
+                    </Grid>
+                    <Grid item  sm="3">
+                        <Card>
+                            <CardHeader  title="Rafta Rafta"  subheader="T series" action={
+                                <IconButton>
+                                    <CancelIcon  sx={{color:colors.primaryColor.color}}/>
+                                </IconButton>
+                            } />
+                            <CardMedia component="img" image="https://img.youtube.com/vi/B-J_PuEhyOM/maxresdefault.jpg"/>
+                        
+                        </Card>
+                    </Grid><Grid item  sm="3">
+                        <Card>
+                            <CardHeader  title="Rafta Rafta"  subheader="T series" action={
+                                <IconButton>
+                                    <CancelIcon  sx={{color:colors.primaryColor.color}}/>
+                                </IconButton>
+                            } />
+                            <CardMedia component="img" image="https://img.youtube.com/vi/B-J_PuEhyOM/maxresdefault.jpg"/>
+                        
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
+export default History;
