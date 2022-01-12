32f9eaec6a16af4c9441945804f673e00d16741a
ubairnoor <ubairnoor5@gmail.com>
2022-01-12 22:14:17 +0530

    Navbar[Mobile Responsive Added and Colors added]

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import colors from '../../colors';
 const useStyles = makeStyles((theme) => ({
     logo: {
         display: 'block',
@@ -25,21 +27,57 @@ const useStyles = makeStyles((theme) => ({
     Toolbar: {
         display: 'flex',
         justifyContent: 'space-between',
-        backgroundColor: '#fff',
+        backgroundColor: colors.whiteColor.color,
         borderBottom: '1px solid #C4C4C4'
     },
     LogInButton: {
-        color: '#FD1863',
+        color: colors.primaryColor.color,
         fontSize: '1.5rem'
 
     },
     SignInButton: {
-        color: '#000036',
+        color: colors.secondaryColor.color,
         fontSize: '1.5rem',
 
     },
     Link: {
         textDecoration: 'none !important'
+    },
+    Search:{
+        display:'flex',
+        alignItems:'center',
+        background:colors.whiteColor.color,
+        borderBottom:'2px solid #000036',
+        borderRadius:1, 
+        width:'20%',
+        alignContent:'center',
+        '&:hover':{
+            borderBottom:'1px solid #000036',
+        },
+        [theme.breakpoints.down('sm')]:{
+            display:(props)=>(props.open ? 'flex':'none'),
+            width:'50%',
+        }
+
+
+    },
+    Buttons:{
+        display:'flex-end',
+        [theme.breakpoints.down('sm')]:{
+            display:'none'
+        }
+    },
+    stack:{
+        color:colors.primaryColor.color,
+        [theme.breakpoints.up('sm')]:{
+            display:'none'
+        },
+        [theme.breakpoints.down('small')]:{
+            display:'block',
+            width:'1rem',
+            height:'1rem'
+
+        }
     }
 
 }));
@@ -55,10 +93,10 @@ const Navbar = () => {
                         <img className={classes.logo} src={logo} alt='Immersis Logo' />
                     </Link>
 
-                    <div>
+                    <div className={classes.Search}>
                         <InputBase placeholder="Search..." />
                     </div>
-                    <div >
+                    <div className={classes.Buttons}>
                         <Link to="/login" className={classes.Link}>
 
                             <Button size="medium" variant='text' className={classes.LogInButton} >Log In</Button>
@@ -68,6 +106,11 @@ const Navbar = () => {
                             <Button size="medium" variant="text" className={classes.SignInButton}>Sign In</Button>
 
                         </Link>
+                    
+
+                    </div>
+                    <div className={classes.stack}>
+                        <ReorderIcon/>
                     </div>
                 </Toolbar>
             </AppBar>
