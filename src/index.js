081c13ff860691397e131d5e53d44c75d442d950
ubairnoor <ubairnoor5@gmail.com>
2022-01-15 18:18:06 +0530

    indemtation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(
-
     <BrowserRouter>
-
         <App />
-    </BrowserRouter>
-
-    ,
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
