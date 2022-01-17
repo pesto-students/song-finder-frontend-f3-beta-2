71ad313369e21380d37ff2c0ba160be9fe74e1a6
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-17 12:33:22 +0530

    add video and audio fetch

--- src/utils/resource.js
+++ src/utils/resource.js
@@ -25,4 +25,46 @@ const fetchResult = (query) => {
     };
 };
 
-export { fetchResult };
+const fetchVideo = (query) => {
+    return async (dispatch) => {
+        dispatch({ type: 'FETCH_VIDEO_REQUEST' });
+
+        const options = {
+            url: `${baseURL}/resource/video?q=${query}`,
+            method: 'GET'
+        };
+
+        try {
+            const resp = await axios(options);
+            dispatch({
+                type: 'FETCH_VIDEO_SUCCESS',
+                payload: resp.data.videoId.videoId
+            });
+        } catch (err) {
+            dispatch({ type: 'FETCH_VIDEO_ERROR', payload: err.message });
+        }
+    };
+};
+
+const fetchAudio = ({ title, artist }) => {
+    return async (dispatch) => {
+        dispatch({ type: 'FETCH_AUDIO_REQUEST' });
+
+        const options = {
+            url: `${baseURL}/resource/audio?title=${title}&artist=${artist}`,
+            method: 'GET'
+        };
+
+        try {
+            const resp = await axios(options);
+            dispatch({
+                type: 'FETCH_AUDIO_SUCCESS',
+                payload: resp.data.url
+            });
+        } catch (err) {
+            dispatch({ type: 'FETCH_AUDIO_ERROR', payload: err.message });
+        }
+    };
+};
+
+export { fetchResult, fetchVideo, fetchAudio };
