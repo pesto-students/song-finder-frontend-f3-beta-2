90efdcc0b3f6c92b2c07674e814461d9fabf0741
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-17 12:32:14 +0530

    add video trigger

--- src/components/UI/Video.js
+++ src/components/UI/Video.js
@@ -1,5 +1,4 @@
 import {
-    Avatar,
     Box,
     Card,
     CardActions,
@@ -10,10 +9,13 @@ import {
 import { makeStyles } from '@material-ui/styles';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import ReceiptTwoToneIcon from '@mui/icons-material/ReceiptTwoTone';
-import SwitchVideoTwoToneIcon from '@mui/icons-material/SwitchVideoTwoTone';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import ReactPlayer from 'react-player/youtube';
+import { fetchVideo } from '../../utils/resource';
 
 const useStyles = makeStyles(() => ({
     // style for Mobile Responsive
@@ -32,57 +34,78 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
-function Video() {
+function VideoFrame({ videoId, title, artist }) {
     const classes = useStyles();
     return (
-        <Box mt={15} align="center">
-            <Container maxWidth="md">
-                <Card>
-                    <CardHeader
-                        avatar={
-                            <Avatar>
-                                <SwitchVideoTwoToneIcon />
-                            </Avatar>
-                        }
-                        title="Rafta Rafta Sanam"
-                        subheader="Atif Aslam"
-                    />
-                </Card>
-                <div className={classes.videResponsive}>
-                    <iframe
-                        width="853"
-                        height="480"
-                        src="https://www.youtube.com/embed/rokGy0huYEA"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay;clipboard-write;encrypted-media;gyroscope;picture"
-                        allowFullScreen
-                        title="Embedded Youtube"
-                    />
-                </div>
-                <CardActions>
-                    <Grid
-                        container
-                        direction="row"
-                        justifyContent="space-evenly"
-                        alignItems="flex-start"
-                    >
-                        <Grid item>
-                            <Tooltip title="Audio" arrow>
-                                <IconButton>
-                                    <MusicNoteIcon />
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Lyrics" arrow>
-                                <IconButton>
-                                    <ReceiptTwoToneIcon />
-                                </IconButton>
-                            </Tooltip>
-                        </Grid>
+        <Container maxWidth="md">
+            <Card>
+                <CardHeader title={title} subheader={artist} />
+            </Card>
+            <div className={classes.videResponsive}>
+                <ReactPlayer
+                    url={`https://www.youtube.com/watch?v=${videoId}`}
+                    height="50%"
+                    width="70%"
+                />
+            </div>
+            <CardActions>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-evenly"
+                    alignItems="flex-start"
+                >
+                    <Grid item>
+                        <Tooltip title="Audio" arrow>
+                            <IconButton>
+                                <MusicNoteIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Lyrics" arrow>
+                            <IconButton>
+                                <ReceiptTwoToneIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
-                </CardActions>
-            </Container>
+                </Grid>
+            </CardActions>
+        </Container>
+    );
+}
+
+function Video({ videoResult, dispatch }) {
+    const navigate = useNavigate();
+    const [params] = useSearchParams();
+    const title = params.get('title');
+    const artist = params.get('artist');
+
+    if (title && artist) {
+        useEffect(() => dispatch(fetchVideo(`${title} ${artist}`)), []);
+    } else {
+        useEffect(() => navigate('/'), []);
+    }
+
+    return (
+        <Box mt={15} align="center">
+            {videoResult.loading ? (
+                <h3>Loading</h3>
+            ) : videoResult.error ? (
+                <h3>{videoResult.error}</h3>
+            ) : (
+                <VideoFrame
+                    videoId={videoResult.id}
+                    title={title}
+                    artist={artist}
+                />
+            )}
         </Box>
     );
 }
 
-export default Video;
+function mapStateToProps(state) {
+    return { videoResult: state.video };
+}
+
+const ConnectedVideo = connect(mapStateToProps)(Video);
+
+export default ConnectedVideo;
