0b6d27aeff9821a8f8260e53c7065218e96db9e7
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-20 19:21:04 +0530

    add resource paths

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Lyrics from './components/UI/Lyrics';
 import Video from './components/UI/Video';
 import Search from './components/UI/Search';
 import Music from './components/UI/Music';
+import History from './components/UI/History';
 
 function App() {
     const font = "'Baloo Bhaijaan 2', cursive;";
@@ -31,8 +32,9 @@ function App() {
                 <Route path="forgotPassword" element={<ForgotPassword />} />
                 <Route path="lyrics" element={<Lyrics />} />
                 <Route path="music" element={<Music />} />
-                <Route path="/video" element={<Video />} />
-                <Route path="/search" element={<Search />} />
+                <Route path="video" element={<Video />} />
+                <Route path="search" element={<Search />} />
+                <Route path="history" element={<History />} />
             </Routes>
             <Footer />
         </ThemeProvider>
