fb59e2baa5dd27d41fde24b561fd2f8079146596
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-24 00:06:52 +0530

    fix after conflict additions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>,
     document.getElementById('root')
 );
