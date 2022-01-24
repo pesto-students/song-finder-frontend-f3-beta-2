3732956a9fd82a288f20421299680546ccb900f3
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-01-24 17:20:56 +0530

    remove console log

--- src/components/UI/NotFound.js
+++ src/components/UI/NotFound.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles(() => ({
 function NotFound() {
     const classes = useStyles();
     const location = useLocation();
-    console.log(location);
+
     return (
         <Box mt={15} mb={30}>
             <Container>
